ea4cdaa89efb1f772ff0d7ad16db4ed01fc6be71
poafs1 <prach.yot@gmail.com>
2023-09-04 22:07:23 +0700

    feat: add sql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigsModule } from './configs/configs.module';
 import { HealthCheckModule } from './health-check/health-check.module';
+import { SqlModule } from './sql/sql.module';
 
 @Module({
-  imports: [ConfigsModule, HealthCheckModule],
+  imports: [ConfigsModule, HealthCheckModule, SqlModule],
   controllers: [AppController],
   providers: [AppService],
 })
