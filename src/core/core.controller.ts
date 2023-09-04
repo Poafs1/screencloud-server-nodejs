a383a60af021f5b05f0e78bbd26e9cce4a96f927
poafs1 <prach.yot@gmail.com>
2023-09-04 22:34:20 +0700

    feat: add api route and spec

--- src/core/core.controller.ts
+++ src/core/core.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CoreService } from './core.service';
+import { AuthDto, AuthInputDto } from './dto/auth.dto';
 
 @Controller('core')
-export class CoreController {}
+export class CoreController {
+  constructor(private readonly coreService: CoreService) {}
+
+  @Post('/auth')
+  async auth(@Body() authInputDto: AuthInputDto): Promise<AuthDto> {
+    return this.coreService.auth(authInputDto);
+  }
+}
--- src/core/core.service.ts
+++ src/core/core.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { AuthDto, AuthInputDto } from './dto/auth.dto';
 
 @Injectable()
-export class CoreService {}
+export class CoreService {
+  async auth(authInputDto: AuthInputDto): Promise<AuthDto> {
+    console.log(authInputDto);
+
+    return {
+      currentBalance: 1000,
+    };
+  }
+}
